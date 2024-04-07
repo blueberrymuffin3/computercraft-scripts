6902d0a1d657ab13148d67ed6d5f0ff8c601b9b8
Evelyn Gordon <evelyn.gordon20@gmail.com>
2024-04-06 20:33:29 -0400

    Improve watchdog error report

--- ea/watchdog.lua
+++ ea/watchdog.lua
@@ -6,17 +6,17 @@ if status then
 else
     print("Program exited with error: \n" .. err)
     print()
-    print("Sending error report to evelyn")
+    print("Sending error report to admin")
     
-    local url = require("secrets").webhookURL
+    local secrets = require("secrets")
     local body = {
-        content="The storage system has crashed <@412111783441072138>\n```\n" .. err .. "\n```",
+        content="`".. os.getComputerLabel() .."` has crashed <@"..secrets.pingUserId..">\n```\n"..err. "\n```",
     }
     local headers = {
         ["Content-Type"]="application/json"
     }
 
-    local response, resError, resFail = http.post(url, textutils.serializeJSON(body), headers)
+    local response, resError, resFail = http.post(secrets.webhookURL, textutils.serializeJSON(body), headers)
     if response == nil then
         print("Failed to send error report:", resError)
         if resFail ~= nil then
