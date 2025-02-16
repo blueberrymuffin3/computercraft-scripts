bc24e69e2549fc8abcb510c9bf4f2fba0613beee
Evelyn Gordon <evelyn.gordon20@gmail.com>
2025-02-16 15:15:12 -0500

    Fix update file

--- ea/startup.lua
+++ ea/startup.lua
@@ -1,36 +1,4 @@
-print("Starting EA in 3 seconds...")
-sleep(3)
+print("Starting EA in 2 seconds...")
+sleep(2)
 
-local networkId = string.match(os.getComputerLabel(), "^(ea_%w+)_.+$")
-local diskLabel = networkId .. "_disk"
-
-local function readFile(path)
-  if not fs.exists(path) then
-    return nil
-  end
-  f = fs.open(path, "r")
-  data = f:readAll()
-  f:close()
-  return data
-end
-
-for _, name in ipairs(peripheral.getNames()) do
-  if peripheral.hasType(name, "drive") and disk.getLabel(name) == diskLabel then
-    local mountPath = disk.getMountPath(name)
-    print("Found disk with label "..diskLabel..": "..name.." mounted at "..mountPath)
-
-    if readFile("/startup") ~= readFile(fs.combine(mountPath, "ea/startup.lua")) then
-      print("Updating startup file...")
-      fs.delete("/startup")
-      fs.copy(fs.combine(mountPath, "ea/startup.lua"), "/startup")
-      print("Rebooting...")
-      sleep(2)
-      os.reboot()
-    end
-
-    shell.run(fs.combine(mountPath, "ea/watchdog.lua"))
-    return
-  end
-end
-
-error("No disk found with label "..diskLabel)
+shell.run("ea/watchdog.lua")
